ac9728647e1af2a7fd40c5a10b64a34dfbe74d46
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-26 16:05:22 -0500

    Update app.js
    Creates function to get coordinates from geonames

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+// Global Data Endpoint
+globalData = {};
 
 // Current Date
 let date = new Date();
@@ -9,7 +11,7 @@ const geonamesUsername = process.env.geonames_username;
 const geonamesBaseURL = 'http://api.geonames.org/searchJSON?q=';
 /* For reference, the full URL needed is as follows:
 'http://api.geonames.org/searchJSON?q="user input city here"&maxRows=2&username="geonamesUsername goes here"
-User Input City will be pulled from client js and geonamesUsername is from .env file */
+ */
 
 
 //WeatherBit Variables
@@ -28,4 +30,15 @@ const pixabayBaseURL = 'https://pixabay.com/api/?key=';
 Note - User input city string must be encoded - see following:
 [https://love2dev.com/blog/whats-the-difference-between-encodeuri-and-encodeuricomponent/] */
 
-async 
+// Function to Get GeoNames API Data
+const retrieveCoordinates = async(baseURL, city, userName) => {
+    const response = await fetch(`${baseURL}${city}&maxRows=2&username=${userName}`)
+    try {
+        const coordData = await response.json();
+        console.log(coordData);
+        return coordData;
+    }
+    catch(error) {console.log(error);}
+};
+
+document.getElementById('generate').addEventListener('click', retrieveCoordinates)
