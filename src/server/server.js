4c898a7c65bd446f4a42f81940c0d01f7377310a
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-26 12:43:51 -0500

    Moves global variables
    Moves global API variables from server.js to newly created app.js

--- src/server/server.js
+++ src/server/server.js
@@ -6,32 +6,9 @@ const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
 
-/* Global Variables */
+
 // Setup empty JS object to acs as endpoint for all routes
 allData = {};
-
-//GeoNames Variables
-const geonamesUsername = process.env.geonames_username;
-const geonamesBaseURL = 'http://api.geonames.org/searchJSON?q=';
-/* For reference, the full URL needed is as follows:
-'http://api.geonames.org/searchJSON?q="user input city here"&maxRows=2&username="geonamesUsername goes here"
-User Input City will be pulled from client js and geonamesUsername is from .env file */
-
-
-//WeatherBit Variables
-const weatherbitApiKey = process.env.weatherbit_api_key;
-const weatherbitBaseURL = 'https://api.weatherbit.io/v2.0/forecast/daily?lat=';
-/* For reference, the full URL needed is as follows:
-'https://api.weatherbit.io/v2.0/forecast/daily?lat="latitude entry"&lon="longitude entry"&key="weatherbitApiKey"' */
-
-//PixaBay Variables
-const pixabayApiKey = process.env.pixabay_api_key;
-const pixabayBaseURL = 'https://pixabay.com/api/?key=';
-/* For reference, the full URL needed is as follows:
-'https://pixabay.com/api/?key="pixabayApiKey"&q="user input city here"&image_type=photo&category=places'
-Note - User input city string must be encoded - see following:
-[https://love2dev.com/blog/whats-the-difference-between-encodeuri-and-encodeuricomponent/] */
-
 
 
 /* Start up an instance of the app */
