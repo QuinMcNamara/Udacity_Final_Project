20eee32497d61faa3f4800b773d8ef07e09cbbc8
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-27 11:22:21 -0500

    Update index.js
    Initialize index.js

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+import {clickHandler, retrieveCoordinates} from './js/app'
+
+import './styles/footer.scss'
+import './styles/main.scss'
+
+document.getElementById('generate').addEventListener('click', clickHandler())
+
+export {
+    clickHandler,
+    retrieveCoordinates
+}
