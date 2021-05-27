5a84d27c845ff81d41f8d349af88acbe7388e2a0
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-27 11:45:09 -0500

    Add rules for .png files

--- webpack.prod.js
+++ webpack.prod.js
@@ -27,7 +27,11 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            }
+            },
+            {
+                test: /\.png$/,
+                loader: 'file-loader'
+            },
         ]
     },
     plugins: [
