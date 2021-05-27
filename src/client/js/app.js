a86e450eaf76a1b6f318c6c5570edc56265b27a3
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-27 08:39:05 -0500

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,6 +1,3 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 /* Global Variables */
 // Global Data Endpoint
 globalData = {};
@@ -10,7 +7,7 @@ let date = new Date();
 let currentDate = (date.getMonth()+1)+'-'+date.getDate+'-'+date.getFullYear();
 
 //GeoNames Variables
-const geonamesUsername = process.env.geonames_username;
+// const geonamesUsername = process.env.geonames_username;
 const geonamesBaseURL = 'http://api.geonames.org/searchJSON?q=';
 /* For reference, the full URL needed is as follows:
 'http://api.geonames.org/searchJSON?q="user input city here"&maxRows=2&username="geonamesUsername goes here"
@@ -18,7 +15,7 @@ const geonamesBaseURL = 'http://api.geonames.org/searchJSON?q=';
 
 
 //WeatherBit Variables
-const weatherbitApiKey = process.env.weatherbit_api_key;
+// const weatherbitApiKey = process.env.weatherbit_api_key;
 const weatherbitForecastBaseURL = 'https://api.weatherbit.io/v2.0/forecast/daily?lat=';
 /* For reference, the full URL needed is as follows:
 'https://api.weatherbit.io/v2.0/forecast/daily?lat="latitude entry"&lon="longitude entry"&key="weatherbitApiKey"' */
@@ -26,7 +23,7 @@ const weatherbitHistoricalBaseURL = 'PLACEHOLDER';
 
 
 //PixaBay Variables
-const pixabayApiKey = process.env.pixabay_api_key;
+// const pixabayApiKey = process.env.pixabay_api_key;
 const pixabayBaseURL = 'https://pixabay.com/api/?key=';
 /* For reference, the full URL needed is as follows:
 'https://pixabay.com/api/?key="pixabayApiKey"&q="user input city here"&image_type=photo&category=places'
@@ -35,7 +32,8 @@ Note - User input city string must be encoded - see following:
 
 // Function to Get GeoNames API Data
 const retrieveCoordinates = async(baseURL, city, userName) => {
-    const response = await fetch(`${baseURL}${city}&maxRows=2&username=${userName}`)
+    // const response = await fetch(`${baseURL}${city}&maxRows=2&username=${userName}`)
+    const response = await fetch(`${baseURL}${city}&maxRows=2&username=quin_mac`)
     try {
         const coordData = await response.json();
         console.log(coordData);
